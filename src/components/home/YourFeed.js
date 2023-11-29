7702d0901ed01b0fed4052a834acc10bda1b6987
pmt1506 <pmtien150603@gmail.com>
2023-11-30 02:20:38 +0700

    fix yourfeed pagination

--- src/components/home/YourFeed.js
+++ src/components/home/YourFeed.js
@@ -1,53 +1,76 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Favorite from "./Favorite";
+import Pagination from "./Pagination";
 
-const YourFeed = ({ currentPage, articlesPerPage }) => {
+const YourFeed = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const articlesPerPage = 10; // Define articlesPerPage as a constant
 
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
-  useEffect(() => {
-    const fetchArticles = async () => {
-      try {
-        setLoading(true);
-        const userToken = localStorage.getItem("userToken"); // Assuming you store the token in localStorage
-        const response = await axios.get(
-          `https://api.realworld.io/api/articles/feed?offset=${(currentPage - 1) * articlesPerPage}`,
-          {
-            headers: {
-              Authorization: `Bearer ${userToken}`,
-            },
-          }
-        );
-        setArticles(response.data.articles);
-      } catch (error) {
-        console.error("Error fetching articles:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchArticles = async (offset) => {
+    try {
+      setLoading(true);
+      const userToken = localStorage.getItem("userToken");
+      const response = await axios.get(
+        `https://api.realworld.io/api/articles/feed?limit=${articlesPerPage}&offset=${offset}`,
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      );
+      setArticles(response.data.articles);
+      setTotalPages(Math.ceil(response.data.articlesCount / articlesPerPage));
+    } catch (error) {
+      console.error("Error fetching articles:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    fetchArticles();
-  }, [currentPage, articlesPerPage]);
+  useEffect(() => {
+    const offset = (currentPage - 1) * articlesPerPage;
+    fetchArticles(offset);
+  }, [currentPage]);
 
   const handleUpdateFavorite = (updatedArticle) => {
-    // Find the index of the updated article in the state
     const updatedIndex = articles.findIndex(
       (article) => article.slug === updatedArticle.slug
     );
 
     if (updatedIndex !== -1) {
-      // Update the state with the modified article
       setArticles((prevArticles) => {
         const newArticles = [...prevArticles];
         newArticles[updatedIndex] = updatedArticle;
         return newArticles;
       });
+    }
+  };
+
+  const handlePageChange = (pageNumber) => {
+    const newOffset = (pageNumber - 1) * articlesPerPage;
+    setCurrentPage(pageNumber);
+    fetchArticles(newOffset);
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      handlePageChange(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      handlePageChange(currentPage + 1);
     }
   };
 
@@ -85,6 +108,15 @@ const YourFeed = ({ currentPage, articlesPerPage }) => {
           </div>
         ))
       )}
+
+      {/* Add Pagination component here */}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages} // Assuming articles.length is the total count
+        handlePageChange={handlePageChange}
+        goToPreviousPage={goToPreviousPage}
+        goToNextPage={goToNextPage}
+      />
     </div>
   );
 };
